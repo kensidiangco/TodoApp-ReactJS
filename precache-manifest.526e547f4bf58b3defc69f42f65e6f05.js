dce8247e5764613a4bbc4f73fc90875b87a9b9b0
kkenn21 <kensidiangco@gmail.com>
2020-07-28 15:38:54 +0800

    Updates

---
+++ precache-manifest.526e547f4bf58b3defc69f42f65e6f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0466af8879668c900a2e85f40ca19674",
+    "url": "/TodoApp-ReactJS/index.html"
+  },
+  {
+    "revision": "638bc732fcc9688143b3",
+    "url": "/TodoApp-ReactJS/static/css/main.ec2e47dd.chunk.css"
+  },
+  {
+    "revision": "1d9179a1cacb53d0ad9d",
+    "url": "/TodoApp-ReactJS/static/js/2.81fb8b04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp-ReactJS/static/js/2.81fb8b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638bc732fcc9688143b3",
+    "url": "/TodoApp-ReactJS/static/js/main.37b3bee9.chunk.js"
+  },
+  {
+    "revision": "ead298ec4e1f0079181e",
+    "url": "/TodoApp-ReactJS/static/js/runtime-main.0146a2e4.js"
+  }
+]);
